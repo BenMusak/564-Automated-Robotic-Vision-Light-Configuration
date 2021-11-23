8c26ec968376acd5162fe7fe26d6ba4c079b1fbf
BenMusak <56428534+BenMusak@users.noreply.github.com>
2021-11-23 13:22:44 +0100

    Changed files to OPCUA changes
    This can now support OPC UA.

---
+++ mainOPCUAExample.py
@@ -0,0 +1,32 @@
+import TCPIP
+import OPCUA
+import setupParameters as sp
+import XMLParser as XMLP
+
+
+def main():
+    communicationChoice = 2
+
+    # if communicationChoice == 1:
+        # TCPIP.tcpIPXMLCommunication()
+    # else:
+
+    # Connect
+    client = OPCUA.connectToClient("opc.tcp://192.168.87.210:4840")
+
+    # Make the profilers with some parameters
+    cameraProfile, barLightProfile1, backlightProfile = sp.setParameters()
+
+    # Make xml profiles and save them to a file
+    xmlData = XMLP.cameraProfilerToXML(cameraProfile, barLightProfile1, backlightProfile)
+    XMLP.parseXMLtoFileAndWrite(xmlData)
+
+    # Send the data to PLC over OPC-UA
+    OPCUA.getRootNode(client)
+    OPCUA.setParameters(client, cameraProfile, barLightProfile1, backlightProfile)
+    OPCUA.setTrigger(client)
+
+
+if __name__ == "__main__":
+    main()
+
