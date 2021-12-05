0b5acf9d4869aa41b056a3693f41df6424a4f688
KajMork <kmark19@student.aau.dk>
2021-12-05 14:07:37 +0100

    Fixed error where list was not sorted by number.

--- folderHandler.py
+++ folderHandler.py
@@ -1,4 +1,5 @@
 import os 
+import re
 
 def getSubFolders():
     folders = os.listdir("B_R_Illumination/static/images")
@@ -6,8 +7,12 @@ def getSubFolders():
 
     images = []
     for i, item in enumerate(folders):
-        images.append(os.listdir("B_R_Illumination/static/images/"+str(folders[i])))
-        #print(images[i])
+        image_list = os.listdir("B_R_Illumination/static/images/"+str(folders[i]))
+        nums = [re.findall('\d+',ss) for ss in image_list] # extracts numbers from strings
+        numsint = [int(*n) for n in nums] # returns 0 for the empty list corresponding to the word
+        sorted_image_list = [x for y, x in sorted(zip(numsint, image_list))] # sorts s based on the sorting of nums2
+        images.append(sorted_image_list)
+        #print(images[i])d
 
     return folders, images
 
