e15b74698b448d7bed7a7aa9ef1adbdf9a7627e3
Andreas Poulsen <andreas.poulsen@br-automation.com>
2022-01-11 15:07:53 +0100

    Added database integration

--- B_R_Illumination/views.py
+++ B_R_Illumination/views.py
@@ -15,18 +15,21 @@ import os
 import route_planner as rp
 import OPCUA
 import xml.etree.cElementTree as ET
-import xmltodict, json
+import xmltodict
+import json
 import re
 import numpy as np
-import json 
+import json
+import mysql.connector
 
 #import roboDK as rDK
 
 response = ""
 folders = []
-images =[]
+images = []
 test_state = False
-run = [False, 0,0,0,0,0,0,0] # Used for stopping the RoboDK threads.We can simulate them using a list, since pointers do not exist in python.
+# Used for stopping the RoboDK threads. We can simulate them using a list, since pointers do not exist in python.
+run = [False, 0, 0, 0, 0, 0, 0, 0]
 firstrun = False
 ROS_Connected = True
 
@@ -35,12 +38,14 @@ try:
 except:
     print("Could not connect to ROS server.")
     ROS_Connected = False
+
 OPCUA_client = OPCUA.connectAsClient("opc.tcp://192.168.87.210:4840")
-#try:
-    #os.remove("camera_route.csv")
-    #os.remove("lightbar_route.csv")
-#except:
-    #pass
+# try:
+# os.remove("camera_route.csv")
+# os.remove("lightbar_route.csv")
+# except:
+# pass
+
 
 @app.route('/process', methods=['POST'])
 def process():
@@ -48,8 +53,8 @@ def process():
     error_msg = ""
     error_state = False
 
-    # Validate the parameter data.   
-    #rp.plan_camera_route([10,10,0], [1,1,1], True) #This is just for testing, should be used in testHandler.py
+    # Validate the parameter data.
+    # rp.plan_camera_route([10,10,0], [1,1,1], True) #This is just for testing, should be used in testHandler.py
     #ih.getURLImage("folder1", "test", "1")
     try:
         camera = request.form['camera']
@@ -64,7 +69,7 @@ def process():
     except:
         barlight1 = "off"
         print("Lightbar = " + barlight1)
-    
+
     try:
         backlight = request.form['backlight']
         print("Backlight = " + backlight)
@@ -86,12 +91,13 @@ def process():
         obj_width = request.form['obj_width']
         obj_length = request.form['obj_length']
         obj_height = request.form['obj_height']
-        print("Object width = " + obj_width + "  Object length = " + obj_length + "  Object height = " + obj_height)
+        print("Object width = " + obj_width + "  Object length = " +
+              obj_length + "  Object height = " + obj_height)
     except:
         obj_height = None
         obj_length = None
         obj_width = None
-    
+
     if obj_height.isdigit():
         print("Value is all good.")
     else:
@@ -104,13 +110,13 @@ def process():
         print("Value is all good.")
     else:
         error_msg = error_msg + " Width contains invalid charachters or is empty. \n"
-    
+
     try:
         view_pointz = request.form['view_pointz']
         print(" Viewpoint z = " + view_pointz)
     except:
         view_pointz = None
-    
+
     if view_pointz.isdigit():
         print("Value is all good.")
     else:
@@ -121,7 +127,7 @@ def process():
         print("Image amount = " + img_amount)
     except:
         img_amount = None
-    
+
     if img_amount.isdigit():
         print("Value is all good.")
     else:
@@ -131,34 +137,38 @@ def process():
         test_name = request.form['test_name']
     except:
         test_name = None
-    
-    if test_name =="":
+
+    if test_name == "":
         error_msg = error_msg + " No test name was given. \n"
     elif os.path.exists("B_R_Illumination/static/XML/" + test_name):
         error_msg = error_msg + " Test name already exist. \n"
     else:
         print("Name is all good.")
 
-    #If data is valid, then begin test. If not or test is already running, then return error message back to the client.
-    if error_msg =="" and test_state == False:
+    # If data is valid, then begin test. If not or test is already running, then return error message back to the client.
+    if error_msg == "" and test_state == False:
         response = "Successfully started the test"
         error_state = False
         test_state = True
-        obj_dim = [int(obj_height)/1000, int(obj_length)/1000, int(obj_width)/1000]
+        obj_dim = [int(obj_height)/1000, int(obj_length) /
+                   1000, int(obj_width)/1000]
         viewPoint = int(view_pointz)/1000
         run[0] = True
-        #Here we call the testing loop.
+        # Here we call the testing loop.
         if ROS_Connected:
-            test_state = th.runTesting(OPCUA_client, ros_client, int(img_amount), test_name, lightColor, backlight, barlight1, camera, obj_dim, viewPoint, run) 
+            test_state = th.runTesting(OPCUA_client, ros_client, int(
+                img_amount), test_name, lightColor, backlight, barlight1, camera, obj_dim, viewPoint, run)
     elif test_state:
         response = "Test is already running."
         error_state = True
     else:
         response = error_msg
         error_state = True
-    return jsonify({'output' : response, 'error_state' : error_state})
+    return jsonify({'output': response, 'error_state': error_state})
 
-#Normal route for returning back to the home-page
+# Normal route for returning back to the home-page
+
+
 @app.route('/')
 @app.route('/home')
 def home():
@@ -169,83 +179,89 @@ def home():
         year=datetime.now().year,
     )
 
-#Route for moving to the folder page.
-@app.route('/folders', methods = ['GET', 'POST'])
+# Route for moving to the folder page.
+
+
+@app.route('/folders', methods=['GET', 'POST'])
 def folder():
     (folders, images) = fh.getSubFolders()
 
-    #We now return the folder page and all the subfolders and filenames.
+    # We now return the folder page and all the subfolders and filenames.
     return render_template(
-        #"test.html",
+        # "test.html",
         "folderViewer.html",
         folders=folders,
         images=images)
 
 
-#Route for moving to the image-viewer page, which depends on the folder nr. that the user clicks on.
-@app.route('/imageViewer/<index>',methods = ['GET', 'POST'])
+# Route for moving to the image-viewer page, which depends on the folder nr. that the user clicks on.
+@app.route('/imageViewer/<index>', methods=['GET', 'POST'])
 def img(index):
-    (folders, images) = fh.getSubFolders() #We get list of subfolders and images in subfolders.
-    #print(folders)
-    #print(folders[int(index)])
-    path = "B_R_Illumination/static/XML/" + folders[int(index)]
-    xml_files = os.listdir(path)
-    nums = [re.findall('\d+',ss) for ss in xml_files] # extracts numbers from strings
-    numsint = [int(*n) for n in nums] # returns 0 for the empty list corresponding to the word
-    sorted_xml_files = [x for y, x in sorted(zip(numsint, xml_files))] # sorts s based on the sorting of nums2
+    # We get list of subfolders and images in subfolders.
+    (folders, images) = fh.getSubFolders()
 
-    print(sorted_xml_files)
+    try:
+        db = mysql.connector.connect(
+            host="localhost", user="andr", password="1212", database="brmysql")
+
+    except:
+        print("Could not connect to database.")
+
+    cursor = db.cursor()
+    cursor.execute("SELECT * FROM images WHERE run = {}".format(index))
+
+    for (id, img_name, path, run, camera_gainLevel, camera_focusScale, camera_exposureTime, camera_flashColor, camera_chromaticLock, camera_irFilter, camera_x, camera_y, camera_z, camera_yaw, camera_pitch, camera_roll, barLight_exposureTime, barLight_flashColor, barLight_angle, barLight_x, barLight_y, barLight_z, barLight_yaw, barLight_pitch, barLight_roll, backLight_exposureTime, backLight_flashColor, passTest) in cursor:
+        settings_list = [img_name, path, run, camera_gainLevel, camera_focusScale, camera_exposureTime, camera_flashColor, camera_chromaticLock, camera_irFilter, camera_x, camera_y, camera_z, camera_yaw, camera_pitch,
+                         camera_roll, barLight_exposureTime, barLight_flashColor, barLight_angle, barLight_x, barLight_y, barLight_z, barLight_yaw, barLight_pitch, barLight_roll, backLight_exposureTime, backLight_flashColor, passTest]
+
     xml_list = []
-    print(len(sorted_xml_files))
-    for file in sorted_xml_files:
-        fullname = os.path.join(path, file)
-        open_file = open(fullname, "r")
-        xml_dict = xmltodict.parse(open_file.read())
-        xml_list.append(xml_dict.copy())
-        open_file.close()
 
-    print(len(xml_list))
-    #print(folders, images)
-    #We now return the image-viewer page and the three necessary variable for determining which subfolder have been chosen.
+    # We now return the image-viewer page and the three necessary variable for determining which subfolder have been chosen.
     return render_template(
         "imageViewer.html",
         folders=folders,
         chosenFolder=index,
         images=images,
-        xml_list=xml_list)
+        settings_list=settings_list)
 
-#Route for changing parameters and starting tests.
-@app.route('/parameters', methods = ['GET', 'POST'])
+# Route for changing parameters and starting tests.
+
+
+@app.route('/parameters', methods=['GET', 'POST'])
 def parameters():
 
-    #We now return the folder page and all the subfolder and filenames.
+    # We now return the folder page and all the subfolder and filenames.
     return render_template(
-        #"test.html",
+        # "test.html",
         "parameters.html"
-        )
+    )
+
 
 @app.route('/statusupdate', methods=['GET'])
 def statusupdate():
 
-    return jsonify({'Teststatus' : run
-     })
+    return jsonify({'Teststatus': run
+                    })
+
 
 @app.route('/createplots', methods=['GET'])
 def createplots():
     try:
         cameraRoute = np.loadtxt("camera_route.csv", delimiter=",").tolist()
     except:
-        cameraRoute=[]
+        cameraRoute = []
     try:
-        lightbarRoute = np.loadtxt("lightbar_route.csv", delimiter=",").tolist()
+        lightbarRoute = np.loadtxt(
+            "lightbar_route.csv", delimiter=",").tolist()
     except:
-        lightbarRoute=[]
+        lightbarRoute = []
 
-    return jsonify({'cameraRoute' : cameraRoute, 'lightbarRoute' : lightbarRoute
-     })
+    return jsonify({'cameraRoute': cameraRoute, 'lightbarRoute': lightbarRoute
+                    })
+
 
 @app.route('/CancelTest', methods=['GET'])
 def cancelTest():
     run[0] = False
     return jsonify({
-     })
+    })
--- testHandler.py
+++ testHandler.py
@@ -1,6 +1,5 @@
 import numpy as np
 import math
-import XMLParser as xmlp
 import controller as con
 import rosbridge as rb
 import imageHandler as ih
@@ -9,6 +8,8 @@ import OPCUA
 import setupParameters as sp
 import time
 from math import sqrt
+import mysql.connector
+
 
 def get_color_nr(argument):
     switcher = {
@@ -17,46 +18,47 @@ def get_color_nr(argument):
         "White": 99,
         "IR": 100
     }
-    return switcher.get(argument, 0) #Return 0 if chosen color does no exist.
+    return switcher.get(argument, 0)  # Return 0 if chosen color does no exist.
 
-def runTesting(OPCUA_client,ros_client, img_amount, foldername, lightcolor, backlight, lightbar, cameralight, obj_hlw, viewpoint, run):
-    #Declaring max and min variables
+
+def runTesting(OPCUA_client, ros_client, img_amount, foldername, lightcolor, backlight, lightbar, cameralight, obj_hlw, viewpoint, run):
+    # Declaring max and min variables
     gain_max = 4
     gain_min = 0
     exposure_max = 1000
     exposure_min = 0
     center = [0.367, 0.120, 0.154]
     light_bar_failed = False
-    #Compute the number of iteration steps for images.
-    #If the lightbar is off we have one less loop to iterate trough and iteration steps for images will therefore be calculated differently.
+    # Compute the number of iteration steps for images.
+    # If the lightbar is off we have one less loop to iterate trough and iteration steps for images will therefore be calculated differently.
     if lightbar == "off":
         x = (img_amount ** (1/3))
     else:
         x = (img_amount ** (1/4))
     print("Number of steps for each loop = ", x)
-    #Convert the iteration steps to gain levels and exposure time.
+    # Convert the iteration steps to gain levels and exposure time.
     gain_steps = (gain_max-gain_min)/x
     exposure_steps = (exposure_max-exposure_min)/x
 
-    # If the lightbar is off we dont need to move the lightbar robotarm. We therefore skip the loop. 
+    # If the lightbar is off we dont need to move the lightbar robotarm. We therefore skip the loop.
     if lightbar == "off":
         light_route = []
         light_route.append(1)
     else:
         light_route = rp.plan_light_route(viewpoint, obj_hlw, False, x, x)
 
-    #Camera route need to run no matter what.
+    # Camera route need to run no matter what.
     camera_route = rp.plan_camera_route(viewpoint, obj_hlw, False, x, x)
 
-    i = 0 #Just for keeping count of total iteration steps.
+    i = 0  # Just for keeping count of total iteration steps.
 
-    #Setup profiles for parameters that are not iterated trough.
+    # Setup profiles for parameters that are not iterated trough.
     cameraprofile = con.CameraProfile()
     barlightprofile = con.BarLightProfile()
     backlightprofile = con.BackLightProfile()
     lightarm_setup = con.barLightSetupProfile()
     cameraarm_setup = con.CameraSetupProfile()
-    
+
     if backlight == "on":
         backlightprofile.flash_color = get_color_nr(lightcolor)
     else:
@@ -73,15 +75,19 @@ def runTesting(OPCUA_client,ros_client, img_amount, foldername, lightcolor, back
     else:
         cameraprofile.flash_color_camera = 0
 
-    #Run the nedsted for loop and iterate over the routes, gain and exposure steps.
+    # Run the nedsted for loop and iterate over the routes, gain and exposure steps.
     for camera_i in camera_route:
-        rb.ROS_SendGoal(ros_client, 1, -0.16, 0.75, "lightbar_robot", viewpoint, obj_hlw) #Move UR5 light to homing position.
-        #input()
+        # Move UR5 light to homing position.
+        rb.ROS_SendGoal(ros_client, 1, -0.16, 0.75,
+                        "lightbar_robot", viewpoint, obj_hlw)
+        # input()
         print("Sending coordinates", camera_i, " to the camera.")
         run[1] = camera_i[0]
         run[2] = camera_i[1]
         run[3] = camera_i[2]
-        UR5_cam_data = rb.ROS_SendGoal(ros_client, camera_i[0],camera_i[1],camera_i[2],"camera_robot", viewpoint, obj_hlw) #Move UR5 cam.
+        # Move UR5 cam.
+        UR5_cam_data = rb.ROS_SendGoal(
+            ros_client, camera_i[0], camera_i[1], camera_i[2], "camera_robot", viewpoint, obj_hlw)
         if run[0] == False:
             break
         if UR5_cam_data["status"]:
@@ -91,22 +97,25 @@ def runTesting(OPCUA_client,ros_client, img_amount, foldername, lightcolor, back
             cameraarm_setup.yaw = UR5_cam_data["rotx"]
             cameraarm_setup.pitch = UR5_cam_data["roty"]
             cameraarm_setup.roll = UR5_cam_data["rotz"]
-            #Computing the distance between object and camera lens.
-            delta_x = (cameraarm_setup.xPos - (center[0]+obj_hlw[2])) 
+            # Computing the distance between object and camera lens.
+            delta_x = (cameraarm_setup.xPos - (center[0]+obj_hlw[2]))
             delta_y = (cameraarm_setup.yPos - (center[1]+obj_hlw[1]))
             delta_z = (cameraarm_setup.zPos - (center[2]+viewpoint))
-            distance = sqrt(pow(delta_x,2)+pow(delta_y,2)+pow(delta_z,2))
-            #Computing the focus. Normally this would be equal to distance, but this camera is not calibrated,
+            distance = sqrt(pow(delta_x, 2)+pow(delta_y, 2)+pow(delta_z, 2))
+            # Computing the focus. Normally this would be equal to distance, but this camera is not calibrated,
             # so we need to compensate with the following formula:
-            cameraprofile.focus_scale = int(-0.00021*pow(distance,2) + 0.908*distance + 4.6845)
-            for light_i in light_route: #TODO: Need to be changed, so that it iterates trough the returned list from plan_light_route.
+            cameraprofile.focus_scale = int(-0.00021 *
+                                            pow(distance, 2) + 0.908*distance + 4.6845)
+            # TODO: Need to be changed, so that it iterates trough the returned list from plan_light_route.
+            for light_i in light_route:
                 if lightbar == "on":
                     if run[0] == False:
                         break
                     run[4] = light_i[0]
                     run[5] = light_i[1]
                     run[6] = light_i[2]
-                    UR5_light_data = rb.ROS_SendGoal(ros_client, light_i[0],light_i[1],light_i[2],"lightbar_robot", viewpoint, obj_hlw)
+                    UR5_light_data = rb.ROS_SendGoal(
+                        ros_client, light_i[0], light_i[1], light_i[2], "lightbar_robot", viewpoint, obj_hlw)
                     if UR5_light_data["status"]:
                         lightarm_setup.xPos = UR5_light_data["x"]
                         lightarm_setup.yPos = UR5_light_data["y"]
@@ -117,9 +126,9 @@ def runTesting(OPCUA_client,ros_client, img_amount, foldername, lightcolor, back
                         light_bar_failed = False
                     else:
                         light_bar_failed = True
-                        
-                #Move on when movement is confirmed.
-                #Maybe even receive pose for the robot arm.
+
+                # Move on when movement is confirmed.
+                # Maybe even receive pose for the robot arm.
                 if light_bar_failed == False:
                     for gain_i in np.arange(gain_min, gain_max, gain_steps):
                         for exposure_i in np.arange(exposure_min, exposure_max, exposure_steps):
@@ -127,35 +136,49 @@ def runTesting(OPCUA_client,ros_client, img_amount, foldername, lightcolor, back
                                 break
                             run[7] = run[7]+1
                             #print("We reached the innr loops")
-                            #TODO Step4: Setup XML file and send to PLC with OPCUA
+                            # TODO Step4: Setup XML file and send to PLC with OPCUA
 
                             cameraprofile.gain_level = int(gain_i)
                             cameraprofile.chromatic_lock = 1
-                            cameraprofile.exposure_time_camera = int(exposure_i)
+                            cameraprofile.exposure_time_camera = int(
+                                exposure_i)
 
                             if lightbar == "on":
                                 barlightprofile.exposure_time = int(exposure_i)
 
                             if backlight == "on":
-                                backlightprofile.exposure_time = int(exposure_i)
+                                backlightprofile.exposure_time = int(
+                                    exposure_i)
 
-                            #Create XML data.
-                            xmlData = xmlp.profilerToXML(cameraprofile, barlightprofile, backlightprofile, cameraarm_setup, lightarm_setup)
-                            #cameraProfile, barLightProfile1, backlightProfile = sp.setParameters()
-
-                            #TODO Step5: Wait for confirmation from PLC that images was captures successfully.
+                            # TODO Step5: Wait for confirmation from PLC that images was captures successfully.
                             OPCUA.getRootNode(OPCUA_client)
-                            OPCUA.setParameters(OPCUA_client, cameraprofile, barlightprofile, backlightprofile)
+                            OPCUA.setParameters(
+                                OPCUA_client, cameraprofile, barlightprofile, backlightprofile)
                             OPCUA.setTrigger(OPCUA_client)
                             print("Triggered camera")
-                            
-                            time.sleep((exposure_i*(pow(10,-6)))*14)
-                            #print((exposure_i*(pow(10,-6)))*14)
-                            #TODO Step6: Retrieve image from the cameras URL.
+
+                            time.sleep((exposure_i*(pow(10, -6)))*14)
+                            # print((exposure_i*(pow(10,-6)))*14)
+                            # TODO Step6: Retrieve image from the cameras URL.
                             ih.getURLImage(foldername, foldername, str(run[7]))
-                            #TODO Step7: Log XML data.
-                            #Save XML data.
-                            xmlp.parseXMLtoFileAndWrite(xmlData, foldername, foldername, str(run[7]))
+
+                            # Upload settings to database
+                            database = mysql.connector.connect(
+                                host="localhost", user="andr", password="1212", database="brmysql")
+                            cursor = database.cursor()
+                            cursor.execute("INSERT INTO images (test_id, path, run, camera_gainLevel, camera_focusScale, camera_exposureTime, " +
+                                           "camera_flashColor, camera_chromaticLock, camera_irFilter, camera_x, camera_y, camera_z, camera_yaw, camera_pitch, camera_roll, " +
+                                           "barLight_exposureTime, barLight_flashColor, barLight_angle, barLight_x, barLight_y, barLight_z, barLight_yaw, barLight_pitch, " +
+                                           "barLight_roll, backLight_exposureTime, backLight_flashColor) VALUES (" +
+                                           "{}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {}, {})".format(foldername, "B_R_Illumination/static/images/" + foldername + "/" + foldername + str(
+                                               run[7]) + ".jpg", run[7],
+                                               cameraprofile.gain_level, cameraprofile.focus_scale, cameraprofile.exposure_time_camera, cameraprofile.flash_color_camera,
+                                               cameraprofile.chromatic_lock, cameraprofile.ir_filter, cameraarm_setup.xPos, cameraarm_setup.yPos, cameraarm_setup.zPos,
+                                               cameraarm_setup.yaw, cameraarm_setup.pitch, cameraarm_setup.roll, barlightprofile.exposure_time, barlightprofile.flash_color,
+                                               barlightprofile.angle, lightarm_setup.xPos, lightarm_setup.yPos, lightarm_setup.zPos, lightarm_setup.yaw, lightarm_setup.pitch,
+                                               lightarm_setup.roll, backlightprofile.exposure_time, backlightprofile.flash_color))
+                            cursor.close()
+
                         else:
                             print("Robotlightbar failed to move to position.")
         else:
@@ -163,6 +186,3 @@ def runTesting(OPCUA_client,ros_client, img_amount, foldername, lightcolor, back
     print(i)
     test_state = False
     return test_state
-
-
-    
